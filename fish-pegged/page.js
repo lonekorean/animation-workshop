12ce3d6f5d4f2cbdf3958a4c56a56e1230feace9
Will Boyd <will@codersblock.com>
2016-09-11 09:24:30 -0400

    Tweak doBusyWork()

--- fish-pegged/page.js
+++ fish-pegged/page.js
@@ -2,9 +2,9 @@ $(function() {
 	$('button').on('click', doBusyWork);
 
 	function doBusyWork() {
-		endTime = Date.now() + 6 * 1000;
+		var endTime = Date.now() + 6 * 1000;
 		while (Date.now() < endTime) {
-			// (>'-')> <('-'<) ^(' - ')^ <('-'<) (>'-')>
+			// churn
 		}
 	}
 });
